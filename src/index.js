820870790998c7728450fbb0263d84c71336a230
wrtinho <tinho1068@hotmail.com>
2020-07-14 21:42:55 -0300

    fib em js (ok)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,47 @@
 'use strict'
 
-const fibonacci = () => null
+const fibonacci = () => {
+  var num=1;
+  var aux=0;
+  var previos=0;
 
-const isFibonnaci = (num) => null
+  var array=[]
+
+  do{
+    array.push(previos)
+
+    num=num+aux;
+    aux=previos;
+    previos=num;
+
+   
+  }while(aux<=350);
+
+    return array
+}
+
+const isFibonnaci = (n) => {
+    var num=1;
+    var aux=0;
+    var previos=0;
+  
+    var array=[]
+    
+        do{
+        array.push(previos)
+    
+        num=num+aux;
+        aux=previos;
+        previos=num;
+    
+        
+        }while(aux<=n);
+        console.log(array)
+    if(array.indexOf(n)){
+        return console.log(true)
+    }
+    return console.log(false)
+}
 
 module.exports = {
     fibonacci,
